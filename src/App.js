fc42c21b0af8bd6c13450d44a68f897c9e4f77fb
mingyukim7608 <71002715+mingyukim7608@users.noreply.github.com>
2023-06-25 01:39:09 +0900

    Where is the build folder?

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/movie/:movieId",
-    element: <Detail />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/movie/:movieId",
+      element: <Detail />,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 function App() {
   return <RouterProvider router={router} />;
